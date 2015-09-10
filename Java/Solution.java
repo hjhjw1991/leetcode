f33a12fc6a8abf73aed79b555842b0aec7ac1745
hjhjw1991 <hjhjw1991@gmail.com>
2015-09-10 17:59:57 +0800

    adding Solution.java which is an abstract class for redirecting input and output.

---
+++ Java/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+//an example of using Test
+public class Solution extends Test{
+    protected void solve(Scanner sc){
+        System.out.println(sc.next());
+    }
+    public static void main(String[] args){
+        Solution so = new Solution();
+        so.run();
+    }
+}
