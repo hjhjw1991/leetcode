790f875e2c66884e653da4a0a6c3605ed534a945
Huang Jun <hjhjw1991@gmail.com>
2016-03-21 04:09:22 +0800

    Add 290 292

---
+++ python/290_Word_Pattern.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        strs = str.split(" ")
+        if len(pattern)!=len(strs):
+            return False
+        dic1={}# pattern to str
+        dic2={}# str to pattern
+        for i in xrange(len(pattern)):
+            if (pattern[i] in dic1 and dic1[pattern[i]]!=strs[i]) or (strs[i] in dic2 and dic2[strs[i]]!=pattern[i]):
+                return False
+            else:
+                dic1[pattern[i]]=strs[i]
+                dic2[strs[i]]=pattern[i]
+        return True
